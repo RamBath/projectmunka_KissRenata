cdb23c49fbc764b9da4bee24f9e07316ccbbc7f8
RenataKiss <fgha108@gmail.com>
2024-05-28 19:42:37 +0200

    initial version

---
+++ adventuregame/src/main/java/be03/kissrenata/App.java
@@ -0,0 +1,13 @@
+package be03.kissrenata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
